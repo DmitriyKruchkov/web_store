8ee7e82a60c0603a9a1befa84232331910812bee
DmitriyKruchkov <kruchkovdmitriy48@yandex.ru>
2024-07-05 15:26:29 +0300

    Добавил енмого логирования

--- backend/database.py
+++ backend/database.py
@@ -2,8 +2,10 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+from backend.core import logger
 from config import DATABASE_URL
 
 Base = declarative_base()
+logger.info(f"creating db:  {DATABASE_URL}")
 engine = create_async_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
