419180767ccedd1397974cb3fda0d50e82fd269d
DmitriyKruchkov <kruchkovdmitriy48@yandex.ru>
2024-07-05 15:29:54 +0300

    Добавил немного логирования

--- backend/core.py
+++ backend/core.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
-from config import origins, REDIS_HOST, REDIS_PORT
+from config import origins, REDIS_HOST, REDIS_PORT, DATABASE_URL
 
 
 app = FastAPI()
@@ -26,3 +26,4 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 logger.info("Redis connecting")
 caching = redis.Redis(REDIS_HOST, REDIS_PORT)
 logger.info("Redis connected")
+logger.info(f"addr db:  {DATABASE_URL}")
